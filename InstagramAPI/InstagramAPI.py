42393922ae6f16a45cb858fee52f38fd5623343c
ronyboo3 <ronyboo333@gmail.com>
2020-02-07 01:21:07 +0900

    Update InstagramAPI.py

--- InstagramAPI/InstagramAPI.py
+++ InstagramAPI/InstagramAPI.py
@@ -61,6 +61,33 @@ class InstagramAPI:
     # isLoggedIn          # Session status
     # rank_token          # Rank token
     # IGDataPath          # Data storage path
+    
+    @classmethod
+    def login_challenge(self, checkpoint_url):
+        BASE_URL = 'https://www.instagram.com/'
+        self.s.headers.update({'Referer': BASE_URL})
+        req = self.s.get(BASE_URL[:-1] + checkpoint_url)
+        self.s.headers.update({'X-CSRFToken': req.cookies['csrftoken'], 'X-Instagram-AJAX': '1'})
+        self.s.headers.update({'Referer': BASE_URL[:-1] + checkpoint_url})
+        challenge_data = {'choice': 1}
+        challenge = self.s.post(BASE_URL[:-1] + checkpoint_url, data=challenge_data, allow_redirects=True)
+        self.s.headers.update({'X-CSRFToken': challenge.cookies['csrftoken'], 'X-Instagram-AJAX': '1'})
+
+        code = input('Enter code received: ').strip()
+        code_data = {'security_code': code}
+        code = self.s.post(BASE_URL[:-1] + checkpoint_url, data=code_data, allow_redirects=True)
+        self.s.headers.update({'X-CSRFToken': code.cookies['csrftoken']})
+        self.cookies = code.cookies
+        code_text = json.loads(code.text)
+        if code_text.get('status') == 'ok':
+            self.authenticated = True
+            self.logged_in = True
+        elif 'errors' in code.text:
+            for count, error in enumerate(code_text['challenge']['errors']):
+                count += 1
+                logging.error('Session error %(count)s: "%(error)s"' % locals())
+        else:
+            logging.error(json.dumps(code_text))
 
     def __init__(self, username, password, debug=False, IGDataPath=None, proxy=None):
         m = hashlib.md5()
@@ -88,32 +115,6 @@ class InstagramAPI:
             print('Set proxy!')
             proxies = {'http': proxy, 'https': proxy}
             self.s.proxies.update(proxies)
-            
-    def login_challenge(self, checkpoint_url):
-        BASE_URL = 'https://www.instagram.com/'
-        self.s.headers.update({'Referer': BASE_URL})
-        req = self.s.get(BASE_URL[:-1] + checkpoint_url)
-        self.s.headers.update({'X-CSRFToken': req.cookies['csrftoken'], 'X-Instagram-AJAX': '1'})
-        self.s.headers.update({'Referer': BASE_URL[:-1] + checkpoint_url})
-        challenge_data = {'choice': 1}
-        challenge = self.s.post(BASE_URL[:-1] + checkpoint_url, data=challenge_data, allow_redirects=True)
-        self.s.headers.update({'X-CSRFToken': challenge.cookies['csrftoken'], 'X-Instagram-AJAX': '1'})
-
-        code = input('Enter code received: ').strip()
-        code_data = {'security_code': code}
-        code = self.s.post(BASE_URL[:-1] + checkpoint_url, data=code_data, allow_redirects=True)
-        self.s.headers.update({'X-CSRFToken': code.cookies['csrftoken']})
-        self.cookies = code.cookies
-        code_text = json.loads(code.text)
-        if code_text.get('status') == 'ok':
-            self.authenticated = True
-            self.logged_in = True
-        elif 'errors' in code.text:
-            for count, error in enumerate(code_text['challenge']['errors']):
-                count += 1
-                logging.error('Session error %(count)s: "%(error)s"' % locals())
-        else:
-            logging.error(json.dumps(code_text))
 
     def login(self, force=False):
         if (not self.isLoggedIn or force):
